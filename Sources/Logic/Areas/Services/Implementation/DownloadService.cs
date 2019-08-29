ba29c9f60af06cff9d1404ab6b52b482bfde4aea
Matthias Müller <matthias.mueller@trivadis.com>
2019-08-29 14:51:42 +0200

    WIP

--- Sources/Logic/Areas/Services/Implementation/DownloadService.cs
+++ Sources/Logic/Areas/Services/Implementation/DownloadService.cs
@@ -10,9 +10,9 @@ namespace Mmu.Wds.Logic.Areas.Services.Implementation
 {
     internal class DownloadService : IDownloadService
     {
+        private readonly ICssAlignmentServant _cssAligner;
         private readonly IFilePathServant _filePathServant;
         private readonly IFileSystem _fileSystem;
-        private readonly ICssAlignmentServant _cssAligner;
         private readonly IWebsitePartHandler[] _partHandlers;
 
         public DownloadService(
@@ -29,29 +29,32 @@ namespace Mmu.Wds.Logic.Areas.Services.Implementation
 
         public async Task DownloadAsync(Uri downloadUri, string targetPath)
         {
-            _filePathServant.CleanPath(targetPath);
-
-            using (var webClient = new WebClient())
+            await Task.Run(() =>
             {
-                var reply = webClient.DownloadString(downloadUri);
-                var htmlDoc = new HtmlDocument();
-                htmlDoc.LoadHtml(reply);
+                _filePathServant.CleanPath(targetPath);
 
-                foreach (var handler in _partHandlers)
+                using (var webClient = new WebClient())
                 {
-                    handler.HandlePart(webClient, htmlDoc, downloadUri, targetPath);
+                    var reply = webClient.DownloadString(downloadUri);
+                    var htmlDoc = new HtmlDocument();
+                    htmlDoc.LoadHtml(reply);
+
+                    foreach (var handler in _partHandlers)
+                    {
+                        handler.HandlePart(webClient, htmlDoc, downloadUri, targetPath);
+                    }
+
+                    _cssAligner.AlignCssFiles(webClient, downloadUri, targetPath);
+
+                    var indexPath = targetPath + @"\index.html";
+                    if (!_fileSystem.Directory.Exists(targetPath))
+                    {
+                        _fileSystem.Directory.CreateDirectory(targetPath);
+                    }
+
+                    htmlDoc.Save(indexPath);
                 }
-
-                _cssAligner.AlignCssFiles(webClient, downloadUri, targetPath);
-
-                var indexPath = targetPath + @"\index.html";
-                if (!_fileSystem.Directory.Exists(targetPath))
-                {
-                    _fileSystem.Directory.CreateDirectory(targetPath);
-                }
-
-                htmlDoc.Save(indexPath);
-            }
+            });
         }
     }
 }
